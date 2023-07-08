308137d2527648cbcb5d04094921e2dd8f08914d
danrod42 <123833557+danrod42@users.noreply.github.com>
2023-07-08 12:19:10 -0400

    per-browser customizations

--- hero-card.js
+++ hero-card.js
@@ -181,15 +181,43 @@
         document.title = 'Unofficial WoW Pandemic Hero - ' + this.textContent;
     });
 
-//    window.addEventListener('beforeprint', function() {
-//        //TODO: print to see how health bar looks
-//        document.querySelector('.hero-card-top').style.backgroundImage = "url('hero-card-template-wip.png')";
-//        var heroCard = document.querySelector('.hero-card');
-//        heroCardBgImage = window.getComputedStyle(heroCard).getPropertyValue('background-image');
-//        heroCard.style.backgroundImage = '';
-//    });
-//
-//    window.addEventListener('afterprint', function() {
-//        document.querySelector('.hero-card').style.backgroundImage = heroCardBgImage;
-//        document.querySelector('.hero-card-top').style.backgroundImage = '';
-//    });
+    // apply per-browser customizations
+    window.addEventListener('load', function() {
+
+        function changeFontSize(element, inc) {
+            const currentFontSize = parseFloat(getComputedStyle(element).fontSize);
+            const newFontSize = currentFontSize + inc;
+            element.style.fontSize = `${newFontSize}px`;
+        }
+
+        function detectBrowser() {
+            let userAgent = navigator.userAgent.toLowerCase();
+            if (userAgent.includes('opr'))
+                return 'opera';
+            if (userAgent.includes('firefox'))
+                return 'firefox';
+            if (userAgent.includes('edg'))
+                return 'edge';
+            if (userAgent.includes('chrome'))
+                return 'chrome';
+            if (userAgent.includes('safari'))
+                return 'safari';
+            return 'other';
+        }
+
+        let perBrowserCustomizations = [
+            {browser: 'firefox', selector: '.hero-name, .hero-quote', fontSize: -1},
+            {browser: 'safari', selector: '.hero-name', fontSize: -2},
+            {browser: 'safari', selector: '.hero-title, .hero-quote', fontSize: -1},
+        ];
+
+        let browser = detectBrowser();
+        perBrowserCustomizations.forEach((customization) => {
+            if (customization.browser === detectBrowser()) {
+                var elements = document.querySelectorAll(customization.selector);
+                for (var i = 0; i < elements.length; i++) {
+                    changeFontSize(elements[i], customization.fontSize);
+                }
+            }
+        });
+    })
